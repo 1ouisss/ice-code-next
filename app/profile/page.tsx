4c92da2150841db952b2e4184d547868edf7eed5
Louis René Maeterlinck <louis.rm3@gmail.com>
2024-12-29 18:09:47 +0100

    Bypass errors to deploy

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,24 +1,24 @@
-import { NextPage } from 'next';
+// app/profile/page.tsx
 
 type ProfileProps = {
-  profile: {
-    id: string;
-    name: string;
-    qrValue: string;
+    profile: {
+      id: string;
+      name: string;
+      qrValue: string;
+    };
   };
-};
-
-// Ensure the component is typed as NextPage and takes ProfileProps as the type
-const ProfilePage: NextPage<ProfileProps> = ({ profile }) => {
-  return (
-    <div className="profile-container">
-      <h2>Your Profile</h2>
-      <p>Unique ID: {profile.id}</p>
-      <p>QR Code: <a href={profile.qrValue}>{profile.qrValue}</a></p>
-      <p>Name: {profile.name}</p>
-      {/* Add more profile data as needed */}
-    </div>
-  );
-};
-
-export default ProfilePage;
+  
+  const ProfilePage = ({ profile }: ProfileProps) => {
+    return (
+      <div className="profile-container">
+        <h2>Your Profile</h2>
+        <p>Unique ID: {profile.id}</p>
+        <p>QR Code: <a href={profile.qrValue}>{profile.qrValue}</a></p>
+        <p>Name: {profile.name}</p>
+        {/* Add more profile data as needed */}
+      </div>
+    );
+  }
+  
+  export default ProfilePage;
+  
